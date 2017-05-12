bcb969a19114db9894d177fbd7e38b3f31365251
DarkWoodens <paulionut2003@gmail.com>
2017-05-12 19:07:21 +0300

    Splus Tree

---
+++ README.md
@@ -0,0 +1,3 @@
+android_device_gionee_wbl7511
+
+Gionee S Plus Device tree (CosmicOS)
---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := WBL7511,Gionee,Splus,S_plus
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
